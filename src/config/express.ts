e81fd73bfdada629cb51a6fafb0be31988ac04cc
Clone <40709168+Cloneee@users.noreply.github.com>
2022-09-29 06:19:41 +0700

    Add logging middleware

--- src/config/express.ts
+++ src/config/express.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import errorHandler from '@middleware/errorHandler';
 import v1Router from '@controller/v1';
+import morgan from 'morgan';
+import fs from 'fs';
 
 const createServer = (): express.Application => {
   const app = express();
@@ -11,6 +13,12 @@ const createServer = (): express.Application => {
   app.use(cors());
   app.use(express.json());
   app.disable('x-powered-by');
+
+  const accessLogStream = fs.createWriteStream(`log/${Date.now()}.log`, {flags: 'a'});
+  app.use(morgan('common', {stream: accessLogStream}));
+  app.use(morgan('dev'));
+
+
   app.use(errorHandler);
 
   app.use('/api/v1', v1Router);
